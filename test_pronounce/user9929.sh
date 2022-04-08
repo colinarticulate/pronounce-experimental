4a9079cc5d227ff19afe8a11e98af892c0d37f7c
pevans9463 <p_a_evans1@hotmail.com>
2022-04-08 16:26:54 +0100

    adding coz git says I need to

--- test_pronounce/user9929.sh
+++ test_pronounce/user9929.sh
@@ -16,8 +16,8 @@ time ./test_pronounce -dict /Users/test/Downloads/sourceFiltered_exp3Mar22.dict 
                         -tests /Users/test/Downloads/user9929/user9929_input.csv \
                         -expectations /Users/test/Downloads/user9929/user9929_expectations1.csv \
                         -outfolder /Users/test/Downloads/audio_temp \
-                        -featparams /Users/test/Documents/GitHub/pronounce-experimental/Models/Bare.ci_cont_24Mar22/feat.params \
-                        -hmm /Users/test/Documents/GitHub/pronounce-experimental/Models/Bare.ci_cont_24Mar22
+                        -featparams /Users/test/Documents/GitHub/pronounce-experimental/Models/Bare.ci_cont_4Apr22/feat.params \
+                        -hmm /Users/test/Documents/GitHub/pronounce-experimental/Models/Bare.ci_cont_4Apr22
 
 
 #time ./test_pronounce -dict /Users/test/Downloads/pronunce_production/dictionary/art_db.dic \
