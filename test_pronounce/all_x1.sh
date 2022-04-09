9a72026adc7446ea1e2e5804ec134fee221686ec
pevans9463 <p_a_evans1@hotmail.com>
2022-04-09 11:51:35 +0100

    minor updates to all_x1 to reflect latest model + latest acoustic model

--- test_pronounce/all_x1.sh
+++ test_pronounce/all_x1.sh
@@ -11,14 +11,14 @@ echo
 #
 
 
-time ./test_pronounce -dict /Users/test/Downloads/sourceFiltered_exp2.dict \
+time ./test_pronounce -dict /Users/test/Downloads/sourceFiltered_exp3Mar22.dict \
  						-phdict /Users/test/Documents/GitHub/pronounce-experimental/Dictionaries/art_db_v3_inference.phone \
                         -infolder /Users/test/go/src/Phonemes/audio_clips/ \
                         -tests /Users/test/go/src/Phonemes/audio_clips/pronounce_input.csv \
                         -expectations /Users/test/go/src/Phonemes/audio_clips/expectations.csv \
                         -outfolder /Users/test/Downloads/audio_temp \
-                        -featparams /Users/test/Documents/GitHub/pronounce-experimental/Models/Bare.ci_cont_2Mar22/feat.params \
-                        -hmm /Users/test/Documents/GitHub/pronounce-experimental/Models/Bare.ci_cont_2Mar22
+                        -featparams /Users/test/Documents/GitHub/pronounce-experimental/Models/Bare.ci_cont_4Apr22/feat.params \
+                        -hmm /Users/test/Documents/GitHub/pronounce-experimental/Models/Bare.ci_cont_4Apr22
                         
 echo
 date
